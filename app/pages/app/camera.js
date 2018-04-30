a947b930ff6aea1ae4760ee186e814edc993d550
huyu <hello_linux@163.com>
2018-04-30 17:27:22 +0800

    增加HTML5 input标签调用相机，摄像机，图库示例

--- app/pages/app/camera.js
+++ app/pages/app/camera.js
@@ -10,7 +10,23 @@ export default class extends Component {
   render() {
     return (
       <div>
-        <h2>调用相机了</h2>
+        <h2>HTML5 input标签调用相机，摄像机，图库示例</h2>
+        <label>
+          <h4>无capture,打开选择框：拍照，照片图库和浏览</h4>
+          <input type="file" accept="image/*" multiple={false} />
+        </label>
+        <label>
+          <h4>capture=camcorder，直接打开相机</h4>
+          <input type="file" accept="image/*" capture="camcorder" />
+        </label>
+        <label>
+          <h4>capture=camcorder，直接打开摄像机(accept属性需设为video/*)</h4>
+          <input type="file" accept="video/*" capture="camcorder" />
+        </label>
+        <label>
+          <h4>无capture,加multiple属性进入图库多选图片(不过不加也可以多选？。)</h4>
+          <input type="file" accept="image/*" multiple />
+        </label>
       </div>
     )
   }
